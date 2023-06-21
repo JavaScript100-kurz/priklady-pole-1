c8a338d857b6089330d3fe82d757ad53276531af
Luděk Roleček <ludek@rolecek.cz>
2023-06-21 17:57:38 +0200

    Aktualizace podkladů

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('JS funguje');
+
+
+// pole
+
+// indexy
+
+// délka
+
+
+// přidávání/odebírání z konce/začátku pole
+
+
+// nahrazování
+
+
+// hledání
+
+
+// cyklus for
+// cyklus for of
+// metoda forEach
